ecde281c4e2c6002ceea28dd9dd924a36c074f16
Oliver Kabi <O.kabi@outlook.com>
2018-02-26 14:54:43 +0000

    Moved testing across to JavaScript to make use of the Mocha testing framework as it is more widely used within Solidity development, meaning more resources available. Basics of escrow contract now developed and test. This was a bigger commit than I would like, but I got distracted following the swap to JS testing and so forgot to commit.

--- contracts/EscrowAgent.sol
+++ contracts/EscrowAgent.sol
@@ -5,11 +5,15 @@ contract EscrowAgent {
     address owner;
     address arbiter;
 
+    event Created(bytes32 escrowHash);
+    event BuyerApproved(address approvedFrom);
+    event SellerApproved(address approvedFrom);
+    event PaidOut(address tranferedTo, uint value);
+
     struct Escrow {
-        bytes16 tradeID;
+        bool active;
         address seller;
         address buyer;
-        uint value;
         uint balance;
         bool buyerApproved;
         bool sellerApproved;
@@ -21,8 +25,38 @@ contract EscrowAgent {
         arbiter = msg.sender;
     }
 
-    function createEscrow(bytes16 tradeID, address seller, address buyer, uint value) public {
-        bytes32 tradeHash = keccak256(tradeID, seller, buyer, value);
-        escrows[tradeHash] = Escrow(tradeID, buyer, seller, value, 0, false, false);
+    function createEscrow(address seller, address buyer) payable external returns (bytes32 escrowHash) {
+        escrowHash = keccak256(seller, buyer, msg.value, now);
+        require(!escrows[escrowHash].active);
+        escrows[escrowHash] = Escrow(true, seller, buyer, msg.value, false, false);
+        Created(escrowHash);
+        return escrowHash;
+    }
+
+    function approve(bytes32 escrowHash) public {
+        Escrow storage escrow = escrows[escrowHash];
+        require(escrow.active);
+        require(msg.sender == escrow.buyer || msg.sender == escrow.seller);
+
+        if (msg.sender == escrow.buyer) {
+            escrow.buyerApproved = true;
+            BuyerApproved(msg.sender);
+        } else if (msg.sender == escrow.seller) {
+            escrow.sellerApproved = true;
+            SellerApproved(msg.sender);
+        }
+
+        if (escrow.buyerApproved && escrow.sellerApproved) {
+            payout(escrowHash);
+        }
+    }
+
+    function payout(bytes32 escrowHash) private {
+        Escrow storage escrow = escrows[escrowHash];
+        require(escrow.active);
+        escrow.seller.transfer(escrow.balance);
+        PaidOut(escrow.seller, escrow.balance);
+        escrow.balance = 0;
+        escrow.active = false;
     }
 }
---
+++ test/escrowAgent.js
@@ -0,0 +1,170 @@
+const EscrowAgent = artifacts.require('./EscrowAgent.sol')
+
+contract('given an escrow agent contract', function(accounts) {
+    let escrowAgent
+
+    let seller = accounts[1]
+    let buyer = accounts[2]
+
+    beforeEach('setup a new contract', async function() {
+        escrowAgent = await EscrowAgent.new()
+    })
+
+    describe('when an escrow is created', async function() {
+        let escrowHash
+
+        beforeEach('create an escrow', async function() {
+            const transactionHash = await escrowAgent.createEscrow(seller, buyer, {from: buyer, value: web3.toWei(0.01, "ether")})
+            escrowHash = transactionHash.logs[0].args.escrowHash
+        })
+
+        describe('that is valid', async function() {
+            it('then emit event with the escrow hash', async function() {
+                assert.isDefined(escrowHash)
+            })
+
+            it('then set the escrow mapped to the escrow hash to exist', async function() {
+                const result = await escrowAgent.escrows.call(escrowHash)
+
+                assert.isTrue(result[0])
+            })
+
+            it('then set the mapped escrow balance to the value sent', async function() {
+                const result = await escrowAgent.escrows.call(escrowHash)
+
+                assert.equal(result[3], web3.toWei(0.01, "ether"))
+            })
+
+            it('then set the mapped escrow seller to the seller specified', async function() {
+                const result = await escrowAgent.escrows.call(escrowHash)
+
+                assert.equal(result[1], seller)
+            })
+
+            it('then set the mapped escrow buyer to the buyer specified', async function() {
+                const result = await escrowAgent.escrows.call(escrowHash)
+
+                assert.equal(result[2], buyer)
+            })
+
+            it('then set the mapped escrow buyerApproved and sellerApproved to false', async function() {
+                const result = await escrowAgent.escrows.call(escrowHash)
+
+                assert.isFalse(result[4])
+                assert.isFalse(result[5])
+            })
+        })
+    })
+
+    describe('when an escrow is approved', function() {
+        let escrowHash
+
+        describe('that exists', function() {
+            beforeEach('create an escrow', async function() {
+                const transactionHash = await escrowAgent.createEscrow(seller, buyer, {from: buyer, value: web3.toWei(0.01, "ether")})
+                escrowHash = transactionHash.logs[0].args.escrowHash
+            })
+
+            describe('by the buyer', function() {
+                it('then set buyerApproved to true', async function() {
+                    escrowAgent.approve(escrowHash, {from: buyer})
+                    const result = await escrowAgent.escrows.call(escrowHash)
+
+                    assert.isTrue(result[4])
+                })
+
+                it('then emit BuyerApproved event', async function() {
+                    let transactionHash = await escrowAgent.approve(escrowHash, {from: buyer})
+
+                    assert.equal(transactionHash.logs[0].event, "BuyerApproved")
+                })
+
+                it('then emit BuyerApproved event with the buyers address', async function() {
+                    let transactionHash = await escrowAgent.approve(escrowHash, {from: buyer})
+                    let approvalAddress = transactionHash.logs[0].args.approvedFrom
+
+                    const escrow = await escrowAgent.escrows.call(escrowHash)
+                    assert.equal(approvalAddress, escrow[2])
+                })
+            })
+
+            describe('by the seller', function() {
+                it('then set sellerApproved to true', async function() {
+                    escrowAgent.approve(escrowHash, {from: seller})
+                    const result = await escrowAgent.escrows.call(escrowHash)
+
+                    assert.isTrue(result[5])
+                })
+
+                it('then emit SellerApproved event', async function() {
+                    let transactionHash = await escrowAgent.approve(escrowHash, {from: seller})
+
+                    assert.equal(transactionHash.logs[0].event, "SellerApproved")
+                })
+
+                it('then emit SellerApproved event with the sellers address', async function() {
+                    let transactionHash = await escrowAgent.approve(escrowHash, {from: seller})
+                    let approvalAddress = transactionHash.logs[0].args.approvedFrom
+
+                    const escrow = await escrowAgent.escrows.call(escrowHash)
+                    assert.equal(approvalAddress, escrow[1])
+                })
+            })
+
+            describe('by an invalid address', function() {
+                it('then it should error', async function() {
+                    try {
+                        await escrowAgent.approve(escrowHash, {from: accounts[3]})
+                    } catch (error) {
+                        assert.equal(error.name, "StatusError")
+                    }
+                })
+
+                it('then approval states should be unchanged', async function() {
+                    const escrow = await escrowAgent.escrows.call(escrowHash)
+                    try {
+                        await escrowAgent.approve(escrowHash, {from: accounts[3]})
+                    } catch (error) {
+                    }
+
+                    const result = await escrowAgent.escrows.call(escrowHash)
+                    assert.equal(result[4], escrow[4])
+                    assert.equal(result[5], escrow[5])
+                })
+            })
+
+            describe('and both buyer and seller have now approved', function() {
+                beforeEach('approve from both buyer and seller', async function() {
+                    await escrowAgent.approve(escrowHash, {from: seller})
+                    await escrowAgent.approve(escrowHash, {from: buyer})
+                })
+
+                it('then the escrow should be paid out to the seller', async function() {
+                    let result
+                    return escrowAgent.allEvents().get(function(err, events) {
+                        result = events.filter(entry => entry.event == "PaidOut")
+                        assert.equal(result[0].args.tranferedTo, seller)
+                    })
+                })
+
+                it('then the balance of the contract should be paid out', async function() {
+                    let result
+                    return escrowAgent.allEvents().get(function(err, events) {
+                        result = events.filter(entry => entry.event == "PaidOut")
+                        assert.equal(result[0].args.value, web3.toWei(0.01, "ether"))
+                    })
+                })
+            })
+        })
+
+        describe('that does not exist', function() {
+            it('then it should error', async function() {
+                try {
+                    await escrowAgent.approve(escrowHash, {from: accounts[5]})
+                } catch (error) {
+                    assert.equal(error.name, "StatusError")
+                }
+            })
+        })
+    })
+})
