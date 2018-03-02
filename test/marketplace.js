144dad4b63a0a890351149f305dafc6956a06a82
Oliver Kabi <O.kabi@outlook.com>
2018-03-02 16:11:28 +0000

    Created tests to ensure added listings are correctly created in the listings map. Due to the use of the time when creating the listing hash, unable to test for cases where the listing already exists, as hash collisions should never happen.

--- test/marketplace.js
+++ test/marketplace.js
@@ -14,13 +14,37 @@ contract('given a marketplace contract', function(accounts) {
         let listingHash
 
         beforeEach('create a listing', async function() {
-            const transactionHash = await marketplace.addListing("item", 0.01, {from: seller})
+            const transactionHash = await marketplace.addListing("item", web3.toWei(0.01, "ether"), {from: seller})
             listingHash = transactionHash.logs[0].args.listingHash
         })
 
         describe('that is valid', async function() {
             it('then emit event with the listing hash', async function() {
                 assert.isDefined(listingHash)
+            })
+
+            it('then set the listing mapped to the listing hash to exist', async function() {
+                const result = await marketplace.listings.call(listingHash)
+
+                assert.isTrue(result[0])
+            })
+
+            it('then set the mapped listing seller to the msg sender', async function() {
+                const result = await marketplace.listings.call(listingHash)
+
+                assert.equal(result[1], seller)
+            })
+
+            it('then set the mapped listing name to the name specified', async function() {
+                const result = await marketplace.listings.call(listingHash)
+
+                assert.equal(result[2], "item")
+            })
+
+            it('then set the mapped listing price to the price specified', async function() {
+                const result = await marketplace.listings.call(listingHash)
+
+                assert.equal(result[3], web3.toWei(0.01, "ether"))
             })
         })
     })
